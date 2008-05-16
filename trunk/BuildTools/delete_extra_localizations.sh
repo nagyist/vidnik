bd8157f122283d10df6b5fb1b2ca32a0d53f57bb
DavidPhillipOster <DavidPhillipOster@3a254cca-fa48-0410-a63a-c12881105b01>
2008-05-16 04:00:22 +0000

    fix bug that broke recording remove redundant internationalizations from Sparkle.

---
+++ trunk/BuildTools/delete_extra_localizations.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd "$1/Contents/Frameworks/Sparkle.framework/Versions/A/Resources"
+rm -rf ca.lproj cs.lproj cy.lproj da.lproj de.lproj es.lproj fi.lproj fr.lproj he.lproj hu.lproj id.lproj is.lproj it.lproj ja.lproj ko.lproj nl.lproj no.lproj pl.lproj ru.lproj sk.lproj sv.lproj th.lproj tr.lproj zh_CN.lproj zh_TW.lproj
